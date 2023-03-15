b81152b02960878584d34108e743ee45e57432c0
rawnly <rawnly@users.noreply.github.com>
2023-03-15 15:02:19 +0100

    feat: add inline description

--- lua/gist/init.lua
+++ lua/gist/init.lua
@@ -2,11 +2,24 @@ local core = require("gist.core.gh")
 
 local M = {}
 
-function M.create()
+function M.create_from_file(args)
 	local config = core.read_config()
 
 	local filename = vim.fn.expand("%:t")
-	local description = vim.fn.input("Description: ")
+	local description = ""
+
+	if args[1] ~= nil then
+		description = args[1]
+	end
+
+	if description == "" or description == nil then
+		vim.api.nvim_echo({
+			{ "No description provided" },
+		}, true, {})
+
+		description = vim.fn.input("Description: ")
+	end
+
 	local is_private = config.is_private or vim.fn.input("Create a private Gist? (y/n): ") == "y"
 
 	local url, err = core.create_gist(filename, nil, description, is_private)
@@ -17,6 +30,12 @@ function M.create()
 		vim.api.nvim_echo({ { "URL (copied to clipboard): " .. url, "Identifier" } }, true, {})
 		vim.fn.setreg(config.clipboard, url)
 	end
+end
+
+function M.create_from_selection(opts)
+	vim.api.nvim_echo({
+		{ "Creating Gist from selection..." .. opts.args, "Identifier" },
+	}, true, {})
 end
 
 return M
--- plugin/gist.lua
+++ plugin/gist.lua
@@ -1,6 +1,18 @@
 local gist = require("gist")
 
-vim.api.nvim_create_user_command("CreateGist", gist.create, {
-	bang = true,
-	desc = "Create a new gist from curretn file",
+vim.api.nvim_create_user_command("Rawnly", function(args)
+	vim.api.nvim_echo({
+		{ "Args: " .. table.concat(args, " "), "Identifier" },
+	}, true, {})
+end, {
+	nargs = "*",
+})
+
+vim.api.nvim_create_user_command("CreateFileGist", gist.create_from_file, {
+	desc = "Create a new gist from current file",
+	nargs = "*",
+})
+
+vim.api.nvim_create_user_command("CreateGist", gist.create_from_selection, {
+	desc = "Create a new gist from current selection",
 })

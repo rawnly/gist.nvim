af34ec0b16647d03dc0d7f5157ed0e5de3663915
lnc3l0t <lnk3@protonmail.com>
2023-05-15 20:01:53 +0200

    improv: changed names to have Gist at the beginning (fixes #11)
    :%s/CreateGist/GistCreate/g
    :%s/ListGists/GistsList/g
    
    Changed all names!

--- plugin/gist.lua
+++ plugin/gist.lua
@@ -1,17 +1,17 @@
 local gist = require("gist.api")
 
-vim.api.nvim_create_user_command("CreateGistFromFile", gist.create_from_buffer, {
+vim.api.nvim_create_user_command("GistCreate", gist.create_from_buffer, {
 	nargs = "?",
 	desc = "Create a Gist from the current file.",
 	range = false,
 })
 
-vim.api.nvim_create_user_command("CreateGist", gist.create_from_file, {
+vim.api.nvim_create_user_command("GistCreateFromFile", gist.create_from_file, {
 	nargs = "?",
 	desc = "Create a Gist from the current selection.",
 	range = true,
 })
 
-vim.api.nvim_create_user_command("ListGists", gist.list_gists, {
+vim.api.nvim_create_user_command("GistsList", gist.list_gists, {
 	desc = "List user Gists.",
 })

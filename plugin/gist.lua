a4f1060d0635b3b0ed3ad6c7558b2dee1d3ba4f7
rawnly <rawnly@users.noreply.github.com>
2023-05-16 15:03:10 +0200

    fix: fix command descriptions and range

--- plugin/gist.lua
+++ plugin/gist.lua
@@ -2,15 +2,14 @@ local gist = require("gist.api")
 
 vim.api.nvim_create_user_command("GistCreate", gist.create_from_buffer, {
 	nargs = "?",
-	desc = "Create a Gist from the current file.",
-	range = false,
+	desc = "Create a Gist from the current buffer selection.",
+	range = true,
 })
-
 
 vim.api.nvim_create_user_command("GistCreateFromFile", gist.create_from_file, {
 	nargs = "?",
-	desc = "Create a Gist from the current selection.",
-	range = true,
+	desc = "Create a Gist from the current buffer.",
+	range = false,
 })
 
 vim.api.nvim_create_user_command("GistsList", gist.list_gists, {

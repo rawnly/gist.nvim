3602ff965b3a0681a466ff00d348056ae1aaf627
Federico <16429579+rawnly@users.noreply.github.com>
2026-01-21 13:57:21 +0100

    chore: recursive config merge

--- lua/gist/utils.lua
+++ lua/gist/utils.lua
@@ -1,17 +1,39 @@
 local M = {}
 
+---@param value any
+---@return boolean
+local function is_table(value)
+  return type(value) == "table" and not vim.islist(value)
+end
+
 ---@generic T: table, K, V
 ---@param defaults T
 ---@param overrides T
 ---@return T
 function M.merge_defaults(defaults, overrides)
+  if overrides == nil then
+    return defaults
+  end
+
   local result = {}
+
   for k, v in pairs(defaults) do
-    result[k] = v
+    if is_table(v) and is_table(overrides[k]) then
+      result[k] = M.merge_defaults(v, overrides[k])
+    elseif overrides[k] ~= nil then
+      result[k] = overrides[k]
+    else
+      result[k] = v
+    end
   end
-  for k, v in pairs(overrides or {}) do
-    result[k] = v
+
+  -- Add keys from overrides that don't exist in defaults
+  for k, v in pairs(overrides) do
+    if result[k] == nil then
+      result[k] = v
+    end
   end
+
   return result
 end
 
